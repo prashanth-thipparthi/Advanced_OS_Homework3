dd82e3f4a7d83ed66f36bef26b1d111eb6dac049
Prashanth Thipparthi <prashanth.reddyt09@gmail.com>
2019-12-02 16:51:47 -0700

    changes

--- work_queue_example.c
+++ work_queue_example.c
@@ -1,17 +1,38 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/workqueue.h>
+#include <linux/sched.h>
+#include <linux/tty.h>
 
 static void mykmod_work_handler(struct work_struct *w);
 
 static struct workqueue_struct *wq = 0;
 static DECLARE_DELAYED_WORK(mykmod_work, mykmod_work_handler);
 static unsigned long onesec;
+static int run_in_loop = 1;
 
-static void
-mykmod_work_handler(struct work_struct *w)
+static void printString(char *string) {
+
+    struct tty_struct *tty;
+
+    tty = get_current_tty();
+
+    if(tty != NULL) { 
+
+        (tty->driver->ops->write) (tty, string, strlen(string));
+    }
+
+    else
+        printk("tty equals to zero");
+}
+
+static void mykmod_work_handler(struct work_struct *w)
 {
-        pr_info("mykmod work %u jiffies\n", (unsigned)onesec);
+        printString("mykmod work jiffies\n");
+        if(run_in_loop && wq) {
+        	queue_delayed_work(wq, &mykmod_work, onesec);
+	}
+	
 }
 
 
@@ -30,6 +51,7 @@ static int __init mykmod_init(void)
 
 static void __exit mykmod_exit(void)
 {
+	run_in_loop = 0;
         if (wq)
                 destroy_workqueue(wq);
         pr_info("mykmod exit\n");
