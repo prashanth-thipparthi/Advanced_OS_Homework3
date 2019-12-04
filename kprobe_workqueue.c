ec4d4efef583f453e24dc01d1a49297bc48244b7
Prashanth Thipparthi <prashanth.reddyt09@gmail.com>
2019-12-04 14:43:55 -0700

    addign kprobe

--- kprobe_workqueue.c
+++ kprobe_workqueue.c
@@ -19,16 +19,16 @@ static struct kprobe kp;
 
 
 static void dump_state(struct pt_regs *regs) {
-    print_symbol(KERN_INFO "EIP is at %s\n", regs->eip);
-    printk(KERN_INFO "eax: %08lx ebx: %08lx ecx: %08lx edx: %08lx\n”, regs->eax, regs->ebx, regs->ecx, regs->edx);
-    printk(KERN_INFO "esi: %08lx edi: %08lx ebp: %08lx esp: %08lx\n”, regs->esi, regs->edi, regs->ebp, regs->esp);
-    printk(KERN_INFO "ds: %04x es: %04x\n”, regs->xds & 0xffff, regs->xes & 0xffff);
-    printk(KERN_INFO "Process %s (pid: %d, threadinfo=%p task=%p)”, current->comm, current->pid,current_thread_info(), current);
+    printk(KERN_INFO "EIP is at %s\n", regs->eip);
+    printk(KERN_INFO "eax: %08lx ebx: %08lx ecx: %08lx edx: %08lx \n", regs->eax, regs->ebx, regs->ecx, regs->edx);
+    printk(KERN_INFO "esi: %08lx edi: %08lx ebp: %08lx esp: %08lx\n", regs->esi, regs->edi, regs->ebp, regs->esp);
+    printk(KERN_INFO "ds: %04x es: %04x\n", regs->xds & 0xffff, regs->xes & 0xffff);
+    printk(KERN_INFO "Process %s (pid: %d, threadinfo=%p task=%p)", current->comm, current->pid,current_thread_info(), current);
 }
 
 /*kprobe pre_handler: called just before the probed instruction is executed*/
 int handler_pre(struct kprobe *p, struct pt_regs *regs) {
-    printk("pre_handler: p->addr=0x%p\n”, p->addr);
+    printk("pre_handler: p->addr=0x%p\n", p->addr);
     dump_state(regs);
     dump_stack();
     return 0;
@@ -36,7 +36,7 @@ int handler_pre(struct kprobe *p, struct pt_regs *regs) {
 
 /*kprobe post_handler: called after the probed instruction is executed*/
 void handler_post(struct kprobe *p, struct pt_regs *regs, unsigned long flags) {
-    printk("post_handler: p->addr=0x%p\n”, p->addr);
+    printk("post_handler: p->addr=0x%p\n", p->addr);
     dump_state(regs);
     return;
 }
@@ -45,8 +45,7 @@ void handler_post(struct kprobe *p, struct pt_regs *regs, unsigned long flags) {
 within the pre- or post-handler, or when Kprobes single-steps the probed instruction.
 */
 int handler_fault(struct kprobe *p, struct pt_regs *regs, int trapnr) {
-    printk("fault_handler: p->addr=0x%p, trap #%dn”,
-    p->addr, trapnr);
+    printk("fault_handler: p->addr=0x%p, trap #%dn",p->addr, trapnr);
     /* Return 0 because we don't handle the fault. */
     return 0;
 }
@@ -94,7 +93,7 @@ static int __init mykmod_init(void)
 	kp.fault_handler = handler_fault;
 	kp.addr = (kprobe_opcode_t*) probe_addr;
 	if (!kp.addr) {
-		printk("Couldn't find the addr to plant kprobe\n”");
+		printk("Couldn't find the addr to plant kprobe\n");
 		return -1;
 	}
 	if ((ret = register_kprobe(&kp) < 0)) {
