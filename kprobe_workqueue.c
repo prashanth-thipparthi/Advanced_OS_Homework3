1c2d9b494481c3354ac3d262e16adface5c1779c
Prashanth Thipparthi <prashanth.reddyt09@gmail.com>
2019-12-04 15:14:52 -0700

    addign kprobe

--- kprobe_workqueue.c
+++ kprobe_workqueue.c
@@ -22,9 +22,9 @@ static struct kprobe kp;
 
 static void dump_state(struct pt_regs *regs) {
     printk(KERN_INFO "EIP is at %s\n", regs->eip);
-    printk(KERN_INFO "eax: %08lx ebx: %08lx ecx: %08lx edx: %08lx \n", regs->eax, regs->ebx, regs->ecx, regs->edx);
-    printk(KERN_INFO "esi: %08lx edi: %08lx ebp: %08lx esp: %08lx\n", regs->esi, regs->edi, regs->ebp, regs->esp);
-    printk(KERN_INFO "ds: %04x es: %04x\n", regs->xds & 0xffff, regs->xes & 0xffff);
+    printk(KERN_INFO "eax: %08lx ebx: %08lx ecx: %08lx edx: %08lx \n", regs->ax, regs->bx, regs->cx, regs->dx);
+    printk(KERN_INFO "esi: %08lx edi: %08lx ebp: %08lx esp: %08lx\n", regs->si, regs->di, regs->bp, regs->sp);
+    printk(KERN_INFO "ds: %04x es: %04x\n", regs->ds & 0xffff, regs->es & 0xffff);
     printk(KERN_INFO "Process %s (pid: %d, threadinfo=%p task=%p)", current->comm, current->pid,current_thread_info(), current);
 }
 
@@ -98,6 +98,7 @@ static int __init mykmod_init(void)
 		printk("Couldn't find the addr to plant kprobe\n");
 		return -1;
 	}
+	int ret = 0;
 	if ((ret = register_kprobe(&kp) < 0)) {
 		printk("register_kprobe failed, returned %d\n", ret); return -1;
 	}
